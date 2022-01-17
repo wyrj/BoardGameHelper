e89959d6596afaba293dafb895fafc1ec95f0f14
wyrj <yrj0103@gmail.com>
2022-01-17 22:07:50 +0800

    feat: add drawer & github link

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,69 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import DicePage from './components/DicePage.vue'
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const drawer = ref<boolean>(false);
 </script>
 
 <template>
-  <DicePage />
+  <el-container class="container">
+    <el-header class="header">
+      <div>
+        <el-icon class="icon" size="30px" @click="drawer = !drawer">
+          <i-ion-menu-sharp />
+        </el-icon>
+      </div>
+      <div>
+        <a
+          class="icon-link"
+          href="https://github.com/wyrj/BoardGameHelper"
+          target="_blank"
+        >
+          <el-icon class="icon" size="30px">
+            <i-ion-logo-github />
+          </el-icon>
+        </a>
+      </div>
+    </el-header>
+    <el-main>
+      <router-view />
+    </el-main>
+    <el-drawer
+      v-model="drawer"
+      direction="ltr"
+      :show-close="false"
+      :with-header="false"
+    >
+    </el-drawer>
+  </el-container>
 </template>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #0b0b0c;
   height: 100vh;
+}
+
+.container {
+  height: 100%;
+}
+
+.header {
+  background-color: var(--el-color-primary-light-8);
+  padding: 15px;
+  display: flex;
+  justify-content: space-between;
+}
+
+.icon {
+  cursor: pointer;
+}
+
+.icon-link {
+  color: inherit;
 }
 
 body {
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
+import DicePage from './views/DicePage.vue'
 
-createApp(App).mount('#app')
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: [
+      { path: '/', component: DicePage },
+  ],
+})
+
+createApp(App)
+  .use(router)
+  .mount('#app')
